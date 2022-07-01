2e0d7d9d52b62a3066f5f57ea79b2796ab8cf831
jesusj <jajm84@gmail.com>
2022-07-01 15:37:38 -0300

    agregue validaciones y formulas

--- app.js
+++ app.js
@@ -25,6 +25,8 @@ const corteCarne5 = {
 
 const cortesElegidos = [];
 const cantidadElegida = [];
+const totalPorCorte = [];
+let totalCompra = 0;
 
 // realizamos la consulta al usuario
 
@@ -39,32 +41,37 @@ let corteCantidad = parseInt(
 );
 cantidadElegida.push(corteCantidad);
 
+
 // creamos el condicional en funcion de lo que escriba el usuario
 
-if (corteCliente === "cuadril") {
+if (corteCliente === "cuadril" && corteCantidad > 0 && corteCantidad != " ") {
   totalCuadril = corteCarne1.precio * corteCantidad;
-  alert(
-    `usted lleva ${corteCantidad}kgs de ${corteCliente} por ${totalCuadril}`
-  );
-} else if (corteCliente === "nalga") {
+  totalPorCorte.push(totalCuadril)
+  console.log(`usted lleva ${corteCantidad}kgs de ${corteCliente} por ${totalCuadril}`)
+
+} else if (corteCliente === "nalga" && corteCantidad > 0 && corteCantidad != " ") {
   totalNalga = corteCarne2.precio * corteCantidad;
-  alert(`usted lleva ${corteCantidad}kgs de ${corteCliente} por ${totalNalga}`);
-} else if (corteCliente === "peceto") {
+  totalPorCorte.push(totalNalga)
+  console.log(`usted lleva ${corteCantidad}kgs de ${corteCliente} por ${totalNalga}`)
+
+} else if (corteCliente === "peceto" && corteCantidad > 0 && corteCantidad != " ") {
   totalPeceto = corteCarne3.precio * corteCantidad;
-  alert(
-    `usted lleva ${corteCantidad}kgs de ${corteCliente} por ${totalPeceto}`
-  );
-} else if (corteCliente === "lomo") {
+  totalPorCorte.push(totalPeceto)
+  console.log(`usted lleva ${corteCantidad}kgs de ${corteCliente} por ${totalPeceto}`)
+
+} else if (corteCliente === "lomo" && corteCantidad > 0 && corteCantidad != " ") {
   totalLomo = corteCarne4.precio * corteCantidad;
-  alert(`usted lleva ${corteCantidad}kgs de ${corteCliente} por ${totalLomo}`);
-} else if (corteCliente === "costilla") {
+  totalPorCorte.push(totalLomo)
+  console.log(`usted lleva ${corteCantidad}kgs de ${corteCliente} por ${totalLomo}`)
+
+} else if (corteCliente === "costilla" && corteCantidad > 0 && corteCantidad != " ") {
   totalCostilla = corteCarne5.precio * corteCantidad;
-  alert(
-    `usted lleva ${corteCantidad}kgs de ${corteCliente} por ${totalCostilla}`
-  );
+  totalPorCorte.push(totalCostilla)
+  console.log(`usted lleva ${corteCantidad}kgs de ${corteCliente} por ${totalCostilla}`)
+
 } else {
   alert(
-    "Opcion incorrecta, debe colocar cuadril, nalga, peceto, lomo o costilla"
+    "Opcion incorrecta, debe colocar cuadril, nalga, peceto, lomo o costilla y en cantidad debe colocar un valor numerico"
   );
   cortesElegidos.pop(); // eliminamos los cortes mal escritos del array
   cantidadElegida.pop(); // eliminamos las cantidades mal escritas del array
@@ -76,51 +83,59 @@ while (confirm("Desea llevar algun corte mas?")) {
   let corteCliente = prompt(
     "Escriba el corte de carne que desee llevar: cuadril, nalga, peceto, lomo o costila"
   );
+  corteCliente.toLowerCase();
   cortesElegidos.push(corteCliente);
+  
   let corteCantidad = parseInt(
-    prompt("Escriba la cantidad de kilos que desea llevar. Minimo 1 kg")
+    prompt("Escriba la cantidad de kilos que desea llevar. Como minimo 1 kg")
   );
   cantidadElegida.push(corteCantidad);
-
-  if (corteCliente === "cuadril") {
+  
+  
+  // creamos el condicional en funcion de lo que escriba el usuario
+  
+  if (corteCliente === "cuadril" && corteCantidad > 0 && corteCantidad != " ") {
     totalCuadril = corteCarne1.precio * corteCantidad;
-    alert(
-      `usted lleva ${corteCantidad}kgs de ${corteCliente} por ${totalCuadril}`
-    );
-  } else if (corteCliente === "nalga") {
+    totalPorCorte.push(totalCuadril)
+    console.log(`usted lleva ${corteCantidad}kgs de ${corteCliente} por ${totalCuadril}`)
+  
+  } else if (corteCliente === "nalga" && corteCantidad > 0 && corteCantidad != " ") {
     totalNalga = corteCarne2.precio * corteCantidad;
-    alert(
-      `usted lleva ${corteCantidad}kgs de ${corteCliente} por ${totalNalga}`
-    );
-  } else if (corteCliente === "peceto") {
+    totalPorCorte.push(totalNalga)
+    console.log(`usted lleva ${corteCantidad}kgs de ${corteCliente} por ${totalNalga}`)
+  
+  } else if (corteCliente === "peceto" && corteCantidad > 0 && corteCantidad != " ") {
     totalPeceto = corteCarne3.precio * corteCantidad;
-    alert(
-      `usted lleva ${corteCantidad}kgs de ${corteCliente} por ${totalPeceto}`
-    );
-  } else if (corteCliente === "lomo") {
+    totalPorCorte.push(totalPeceto)
+    console.log(`usted lleva ${corteCantidad}kgs de ${corteCliente} por ${totalPeceto}`)
+  
+  } else if (corteCliente === "lomo" && corteCantidad > 0 && corteCantidad != " ") {
     totalLomo = corteCarne4.precio * corteCantidad;
-    alert(
-      `usted lleva ${corteCantidad}kgs de ${corteCliente} por ${totalLomo}`
-    );
-  } else if (corteCliente === "costilla") {
+    totalPorCorte.push(totalLomo)
+    console.log(`usted lleva ${corteCantidad}kgs de ${corteCliente} por ${totalLomo}`)
+  
+  } else if (corteCliente === "costilla" && corteCantidad > 0 && corteCantidad != " ") {
     totalCostilla = corteCarne5.precio * corteCantidad;
-    alert(
-      `usted lleva ${corteCantidad}kgs de ${corteCliente} por ${totalCostilla}`
-    );
+    totalPorCorte.push(totalCostilla)
+    console.log(`usted lleva ${corteCantidad}kgs de ${corteCliente} por ${totalCostilla}`)
+  
   } else {
     alert(
-      "Opcion incorrecta, debe colocar cuadril, nalga, peceto, lomo o costilla"
+      "Opcion incorrecta, debe colocar cuadril, nalga, peceto, lomo o costilla y en cantidad debe colocar un valor numerico"
     );
-    cortesElegidos.pop();
-    cantidadElegida.pop();
+    cortesElegidos.pop(); // eliminamos los cortes mal escritos del array
+    cantidadElegida.pop(); // eliminamos las cantidades mal escritas del array
   }
+  
 }
 
-console.log(cortesElegidos);
-console.log(cantidadElegida);
+// realizamos la suma de los montos por corte elegido por el usuario
 
-for (const corteCliente of cortesElegidos) {
-  console.log(`usted esta comprando ${corteCliente}`);
+
+for (let i = 0; i < totalPorCorte.length; i++) {
+    totalCompra += totalPorCorte[i]
+    
 }
 
+console.log(`el total de su compra es ${totalCompra}`)
 console.log("Gracias por su compra");
